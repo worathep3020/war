656d0de1be20bbbcde47cfaa4cf7f67b135062fb
worathep3020 <worathep@users.noreply.github.com>
2020-08-18 13:06:20 +0700

    git init

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/">
+ <div id="app">
+   
+    <img alt="Vue logo" src="./assets/vs.png" width="300">
+    <HelloWorld msg="Apocalypse War"/>
+    <div class="row">
+      <div class="col-12"><game Start="Start" End="End"/>
+
+      </div>
+    </div>
+   </div>
+    
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+import game from './components/game.vue'
+
 export default {
   name: 'App',
   components: {
-    
+    HelloWorld,
+    game
     
   },
+  
   randomDamage: function(min, max) {             
     return Math.max(Math.floor(Math.random() * max) + 1, min);    
     },
-    
+  data :function(){
+    return{
+    total:0
+    }
+
+  },
+  methods:{
+    sum(counterValue){
+      this.total += counterValue
+
+    },
+  },
+  
 }
 
 </script>
@@ -27,3 +54,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+
---
+++ src/components/game.vue
@@ -0,0 +1,62 @@
+<template>
+  <div >
+    <button @click="upFunction" class="btn btn-info">{{ Start }}</button>
+    <p v-bind:style="{ fontSize: fsize + 'px' }">{{ counter }}</p>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      counter: 0,
+      fsize: 50,
+      player:[
+          {name:'A', hp:100 , image: './assets/Anakin.jpg'},
+          {name:'D', hp:300 , image: './assets/Doraemon.jpg'},
+          {name:'Ir', hp:100 , image: './assets/Ir.jpg'},
+      ],
+      monster:[
+          {name:'A', hp:100 , image: ''},
+          {name:'A', hp:100 , image: ''},
+          {name:'A', hp:100 , image: ''},
+      ]
+        
+      
+    };
+  },
+
+  props: {
+    Start: String,
+    End: String,
+    
+  },
+
+  methods: {
+    upFunction: function () {
+      this.counter += 1;
+      this.fsize += 10;
+
+      //console.log('fsize =' +this.fsize);
+      //console.log('counter =' +this.counter);
+
+      //ส่งค่าตัวแปร counter ออกไปโดยผ่านตัวแปร cc
+      this.$emit('cc',this.counter)
+
+    },
+
+    downFunction: function () {
+      this.counter -= 1;
+      this.fsize -= 10;
+
+      console.log(this.fsize);
+      console.log(this.counter);
+    },
+  },
+};
+</script>
+
+
+<style>
+</style>
